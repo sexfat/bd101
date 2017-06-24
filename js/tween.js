e5261a21516c8484a9dde47a250791131da28f26
sexfat <bryant6831@gmail.com>
2017-06-24 16:37:58 +0800

    add tweenmax fromto

--- js/tween.js
+++ js/tween.js
@@ -1,1 +1,23 @@
-//consolo
+$(function () {
+    TweenMax.fromTo('.title', 1, {
+        x : -20,
+        y: -100,
+        opacity: 0
+    }, {
+        x: 0,
+        y: 0,
+        opacity: 1,
+        ease: Power3.easeOut
+    });
+    TweenMax.fromTo('.content', 1, {
+        x: -150,
+        opacity: 0
+    }, {
+        x: 0,
+        opacity: 1,
+        delay : 1,
+        ease: Elastic.easeOut.config(1, 0.3)
+    });
+
+    console.log('tween ok');
+});
